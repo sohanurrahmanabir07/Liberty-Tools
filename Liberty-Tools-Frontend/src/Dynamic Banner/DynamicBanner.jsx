f54cfe57aa5291bb7c786e3ab18494c0c9a805d8
sohanurrahmanabir07 <mdsohanurrahmanabir@gmail.com>
2025-07-07 12:18:57 +0600

    Updated the Category Banner

--- Liberty-Tools-Frontend/src/Dynamic Banner/DynamicBanner.jsx
+++ Liberty-Tools-Frontend/src/Dynamic Banner/DynamicBanner.jsx
@@ -8,7 +8,7 @@ export const DynamicBanner = ({ item }) => {
     AOS.init()
   }, [])
   return (
-    <div>
+    <div >
       {
 
         !item ?
@@ -17,10 +17,10 @@ export const DynamicBanner = ({ item }) => {
           )
           :
           (
-            <div data-aos="fade-up"
-              data-aos-duration="1000" className='relative overflow-hidden rounded-lg max-sm:h-[200px] max-sm:rounded-none shadow-sm shadow-gray-400 -mt-20'>
+            <div className='relative overflow-hidden rounded-lg max-sm:h-[200px] max-sm:rounded-none shadow-sm shadow-gray-400 -mt-20'>
 
-              <img loading="lazy" src={` ${item?.bannerImgUrl[0] || `https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/800px-Image_not_available.png?20210219185637`} `} className='h-full w-full' alt="" />
+              <img data-aos="fade-up"
+                data-aos-duration="2000" loading="lazy" src={` ${item?.bannerImgUrl[0] || `https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/800px-Image_not_available.png?20210219185637`} `} className='h-full w-full' alt="" />
               <div className='absolute  bottom-5 left-5'>
                 <p className='font-semibold text-3xl text-gray-300 text-shadow-xl text-shadow-yellow-600'>
                   {capitalizeWords(item?.name)}
